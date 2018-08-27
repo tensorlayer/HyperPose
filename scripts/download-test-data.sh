429d4f7a33ef31367365637a2d7ac881ade96e0e
lg <lg4869@outlook.com>
2018-08-27 15:48:45 +0800

    fix download script

--- scripts/download-test-data.sh
+++ scripts/download-test-data.sh
@@ -6,4 +6,4 @@ cd $(dirname $0)/..
 mkdir -p data
 
 # download data/test.jpeg
-curl https://github.com/CMU-Perceptual-Computing-Lab/openpose/raw/master/examples/media/COCO_val2014_000000000192.jpg >data/test.jpeg
+curl -L https://github.com/CMU-Perceptual-Computing-Lab/openpose/raw/master/examples/media/COCO_val2014_000000000192.jpg >data/test.jpeg
