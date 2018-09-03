d6d94b603d7081ff5f928e606d867d4a921d019d
lg <lg4869@outlook.com>
2018-09-03 23:09:00 +0800

    revert

--- utils.py
+++ utils.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+## xxx
 import math
 import os
 from distutils.dir_util import mkpath
@@ -13,13 +14,10 @@ from config import config
 from pycocotools.coco import COCO, maskUtils
 from tensorlayer import logging
 from tensorlayer.files.utils import (del_file, folder_exists, maybe_download_and_extract)
-import tensorflow as tf
-import tensorlayer as tl
 
 n_pos = config.MODEL.n_pos
 hout = config.MODEL.hout
 wout = config.MODEL.wout
-
 
 ## download dataset
 def load_mscoco_dataset(path='data', dataset='2017', task='person'):  # TODO move to tl.files later
@@ -694,46 +692,6 @@ def draw_results(images, heats_ground, heats_result, pafs_ground, pafs_result, m
 
         mkpath(config.LOG.vis_path)
         plt.savefig(os.path.join(config.LOG.vis_path, '%s%d.png' % (name, i)), dpi=300)
-
-def vis_annos(image, annos,name=''):
-    """Save results for debugging.
-
-    Parameters
-    -----------
-    images : single RGB image
-    annos  : annotation, list of lists
-    """
-
-    fig = plt.figure(figsize=(8, 8))
-    a = fig.add_subplot(1, 1, 1)
-
-    plt.imshow(image)
-    for people in annos:
-        for idx, jo in enumerate(people):
-            if jo[0] > 0 and jo[1] > 0:
-                plt.plot(jo[0], jo[1], '*')
-
-    mkpath(config.LOG.vis_path)
-    plt.savefig(os.path.join(config.LOG.vis_path, 'keypoints%s%d.png' % (name, i)), dpi=300)
-
-def load_image(input_file):
-    image_height = 368
-    image_width = 432
-
-    im = tl.vis.read_image(input_file)
-    im = tl.prepro.imresize(im, [image_height, image_width])
-    im = im / 255.  # input image 0~1
-    return im
-
-
-def get_peak(pafs_tensor):
-    from inference.smoother import Smoother
-    smoother = Smoother({'data': pafs_tensor}, 25, 3.0)
-    gaussian_heatMat = smoother.get_output()
-    max_pooled_in_tensor = tf.nn.pool(gaussian_heatMat, window_shape=(3, 3), pooling_type='MAX', padding='SAME')
-    tensor_peaks = tf.where(
-        tf.equal(gaussian_heatMat, max_pooled_in_tensor), gaussian_heatMat, tf.zeros_like(gaussian_heatMat))
-    return tensor_peaks
 
 
 if __name__ == '__main__':
