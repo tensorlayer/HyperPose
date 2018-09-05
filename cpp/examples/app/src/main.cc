ca066b40cf0e3542c0ea82d7fa195e0a072debaf
lg <lg4869@outlook.com>
2018-09-06 01:24:47 +0800

    disable camera example

--- cpp/examples/app/src/main.cc
+++ cpp/examples/app/src/main.cc
@@ -13,25 +13,25 @@
 DEFINE_string(graph_path, "", "path to freezed graph.");
 DEFINE_string(input_images, "", "Comma separated list of image filenames.");
 
-void camera_example()
-{
-    cv::VideoCapture cap(0);
-    if (!cap.isOpened()) { return; }
-    cv::Mat edges;
-    cv::namedWindow("edges", 1);
-    for (int i = 0;; ++i) {
-        printf("#%d\n", i);
-        cv::Mat frame;
-        cap >> frame;  // get a new frame from camera
-        cv::cvtColor(frame, edges, CV_BGR2GRAY);
-        cv::GaussianBlur(edges, edges, cv::Size(7, 7), 1.5, 1.5);
-        cv::Canny(edges, edges, 0, 30, 3);
-        cv::imshow("edges", edges);
-        if (cv::waitKey(30) >= 0) { break; }
-        const std::string name = "frame-" + std::to_string(i) + ".png";
-        cv::imwrite(name, frame);
-    }
-}
+// void camera_example()
+// {
+//     cv::VideoCapture cap(0);
+//     if (!cap.isOpened()) { return; }
+//     cv::Mat edges;
+//     cv::namedWindow("edges", 1);
+//     for (int i = 0;; ++i) {
+//         printf("#%d\n", i);
+//         cv::Mat frame;
+//         cap >> frame;  // get a new frame from camera
+//         cv::cvtColor(frame, edges, CV_BGR2GRAY);
+//         cv::GaussianBlur(edges, edges, cv::Size(7, 7), 1.5, 1.5);
+//         cv::Canny(edges, edges, 0, 30, 3);
+//         cv::imshow("edges", edges);
+//         if (cv::waitKey(30) >= 0) { break; }
+//         const std::string name = "frame-" + std::to_string(i) + ".png";
+//         cv::imwrite(name, frame);
+//     }
+// }
 
 void pose_example(const std::vector<std::string> &image_files)
 {
