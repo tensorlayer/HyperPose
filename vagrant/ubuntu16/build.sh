db0b211f9cfde33225f93e8caaf97f91aeedd6d4
lg <lg4869@outlook.com>
2018-09-05 19:06:22 +0800

    install python3

--- vagrant/ubuntu16/build.sh
+++ vagrant/ubuntu16/build.sh
@@ -2,7 +2,10 @@
 set -e
 
 cd
-git clone https://github.com/tensorlayer/openpose.git
+
+[ ! -d openpose ] && git clone https://github.com/tensorlayer/openpose.git
 cd openpose
 git checkout cpp
+
+export PYTHON_BIN_PATH=$(which python3)
 make
--- vagrant/ubuntu16/provision.sh
+++ vagrant/ubuntu16/provision.sh
@@ -5,4 +5,4 @@ cp /vagrant/sources.list.ustc /etc/apt/sources.list
 echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl -s https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
 apt update
-apt install -y g++ cmake bazel
+apt install -y g++ cmake bazel python3 python3-pip
