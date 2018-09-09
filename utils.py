2ca37c4d54bf9a3095e33ff159ab960e1511d609
boldjoel <boldjoel@gmail.com>
2018-09-09 16:29:15 +0100

    add tf_repeat

--- utils.py
+++ utils.py
@@ -713,6 +713,24 @@ def vis_annos(image, annos,name=''):
 
     mkpath(config.LOG.vis_path)
     plt.savefig(os.path.join(config.LOG.vis_path, 'keypoints%s%d.png' % (name, i)), dpi=300)
+def tf_repeat(tensor, repeats):
+    """
+    Args:
+
+    input: A Tensor. 1-D or higher.
+    repeats: A list. Number of repeat for each dimension, length must be the same as the number of dimensions in input
+
+    Returns:
+
+    A Tensor. Has the same type as input. Has the shape of tensor.shape * repeats
+    """
+
+    expanded_tensor = tf.expand_dims(tensor, -1)
+    multiples = [1] + repeats
+    tiled_tensor = tf.tile(expanded_tensor, multiples=multiples)
+    repeated_tesnor = tf.reshape(tiled_tensor, tf.shape(tensor) * repeats)
+
+    return repeated_tesnor
 
 if __name__ == '__main__':
     data_dir = '/Users/Joel/Desktop/coco'
