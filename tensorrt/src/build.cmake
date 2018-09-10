3c395b1ce6f2618b7d37e3eb1d2460d8a577843c
lg <lg4869@outlook.com>
2018-09-11 00:46:18 +0800

    fix cmake

--- tensorrt/src/build.cmake
+++ tensorrt/src/build.cmake
@@ -14,6 +14,7 @@ TARGET_LINK_LIBRARIES(vis opencv_core opencv_imgproc opencv_highgui)
 
 ADD_EXECUTABLE(runner
                ${CMAKE_CURRENT_LIST_DIR}/uff-runner.cpp
+               ${CMAKE_CURRENT_LIST_DIR}/tracer.cpp
                ${CMAKE_CURRENT_LIST_DIR}/main.cpp
                ${CMAKE_CURRENT_LIST_DIR}/cuda_buffer.cpp)
 TARGET_LINK_LIBRARIES(
