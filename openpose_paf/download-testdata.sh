f9a725b54ade533f465b82aefbfcacb7f88781bc
lg <lg4869@outlook.com>
2018-09-13 22:46:49 +0800

    fix

--- openpose_paf/download-testdata.sh
+++ openpose_paf/download-testdata.sh
@@ -3,7 +3,7 @@ set -e
 
 cd $(dirname $0)
 if [ ! -f network-outputs ]; then
-    time curl -sLOJ https://github.com/tensorlayer/fast-openpose/files/2378505/network-outputs.gz
+    curl -sLOJ https://github.com/tensorlayer/fast-openpose/files/2378505/network-outputs.gz
     gzip -d network-outputs.gz
 fi
 tar -xf network-outputs
