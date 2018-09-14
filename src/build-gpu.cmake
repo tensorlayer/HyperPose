3a299d2a2310343f18e76d3d268b87a44f4fbd2a
lg <lg4869@outlook.com>
2018-09-14 23:08:06 +0800

    fix missing dependencies

--- src/build-gpu.cmake
+++ src/build-gpu.cmake
@@ -1,3 +1,8 @@
+# FIXME: use TARGET_LINK_DIRECTORIES and TARGET_INCLUDE_DIRECTORIES
+LINK_DIRECTORIES(/usr/local/cuda-9.0/targets/x86_64-linux/lib)
+INCLUDE_DIRECTORIES(/usr/local/cuda-9.0/targets/x86_64-linux/include
+                    /usr/local/cuda-9.0/targets/x86_64-linux/include/crt)
+
 ADD_EXECUTABLE(runner
                ${CMAKE_CURRENT_LIST_DIR}/uff-runner.cpp
                ${CMAKE_CURRENT_LIST_DIR}/tracer.cpp
