cc89e00b3fe8290d9cdb389e3eab08989ca70092
lg <lg4869@outlook.com>
2018-09-27 02:06:41 +0800

    remove unused files

--- src/build-gpu.cmake
+++ src/build-gpu.cmake
@@ -1,7 +1,8 @@
 FIND_PACKAGE(CUDA REQUIRED)
 
-SET(CUDA_RT /usr/local/cuda-9.0/targets/x86_64-linux)
-# FIXME: use TARGET_LINK_DIRECTORIES and TARGET_INCLUDE_DIRECTORIES
+SET(CUDA_RT /usr/local/cuda-9.0/targets/x86_64-linux) # FIXME: auto detect it
+
+# FIXME: use TARGET_LINK_DIRECTORIES and TARGET_INCLUDE_DIREC TORIES
 LINK_DIRECTORIES(${CUDA_RT}/lib)
 INCLUDE_DIRECTORIES(${CUDA_RT}/include ${CUDA_RT}/include/crt)
 
@@ -14,17 +15,14 @@ TARGET_LINK_LIBRARIES(cudnn++ cudnn cudart)
 ADD_LIBRARY(post-process_cpu ${CMAKE_CURRENT_LIST_DIR}/post-process.cpp)
 TARGET_LINK_LIBRARIES(post-process_cpu)
 
-ADD_LIBRARY(post-process_gpu ${CMAKE_CURRENT_LIST_DIR}/post-process_gpu.cpp)
-TARGET_LINK_LIBRARIES(post-process_gpu post-process_cpu cudnn++)
-
 ADD_LIBRARY(paf ${CMAKE_CURRENT_LIST_DIR}/paf.cpp)
-TARGET_LINK_LIBRARIES(paf tracer post-process_cpu post-process_gpu)
+TARGET_LINK_LIBRARIES(paf tracer post-process_cpu cudnn++)
 
 ADD_LIBRARY(uff-runner ${CMAKE_CURRENT_LIST_DIR}/uff-runner.cpp)
 TARGET_LINK_LIBRARIES(uff-runner cuda-buffer nvinfer nvparsers)
 
 ADD_LIBRARY(pose-detetor ${CMAKE_CURRENT_LIST_DIR}/pose_detector.cpp)
-TARGET_LINK_LIBRARIES(pose-detetor uff-runner post-process_gpu input_image paf)
+TARGET_LINK_LIBRARIES(pose-detetor uff-runner input_image paf)
 
 ADD_EXECUTABLE(example ${CMAKE_CURRENT_LIST_DIR}/example.cpp)
 TARGET_LINK_LIBRARIES(example tracer pose-detetor vis gflags)
--- src/post-process_gpu.cpp
+++
@@ -1,7 +0,0 @@
-#include "post-process_gpu.h"
-
-#include <cassert>
-
-#include "cudnn.hpp"
-#include "post-process.h"
-#include "tracer.h"
