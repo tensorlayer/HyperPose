3787cabb7e6ea504ef6bdf15d6d0538477938c49
lg <lg4869@outlook.com>
2018-09-30 10:12:41 +0800

    init

--- src/stream_detector.h
+++ src/stream_detector.h
@@ -2,10 +2,24 @@
 #include <string>
 #include <vector>
 
+#include "human.h"
+
 class stream_detector
 {
   public:
+    struct inputer_t {
+        virtual bool operator()(int height, int width, uint8_t *hwc_ptr,
+                                float *chw_ptr) = 0;
+    };
+
+    struct handler_t {
+        virtual void operator()(cv::Mat &image,
+                                const std::vector<human_t> &humans) = 0;
+    };
+
     virtual ~stream_detector() {}
+
+    virtual void run(inputer_t &, handler_t &, int count) = 0;
 
     virtual void run(const std::vector<std::string> &) = 0;
 
