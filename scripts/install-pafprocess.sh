de907f9b10971c1250e2e9b049537b330daa96c7
mandeman <abhijitmande1024@gmail.com>
2018-10-22 21:23:22 -0400

    Update install-pafprocess.sh
    Link is broken. Please check if the link I have added is the same.

--- scripts/install-pafprocess.sh
+++ scripts/install-pafprocess.sh
@@ -3,7 +3,7 @@ set -e
 
 cd $(dirname $0)/../openpose_plus/inference
 
-[ ! -d pafprocess ] && svn export https://github.com/ildoonet/tf-pose-estimation/trunk/tf_pose/pafprocess
+[ ! -d pafprocess ] && svn export https://github.com/ildoonet/tf-pose-estimation/tree/master/tf_pose/pafprocess
 cd pafprocess
 
 swig -python -c++ pafprocess.i
