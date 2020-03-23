55bd993e8e7caeb1da154ee43b9c3a89a1e9315d
ganler <jaway.liu@gmail.com>
2020-03-23 11:16:02 +0800

    Fix script

--- scripts/install-pafprocess.sh
+++ scripts/install-pafprocess.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -e
 
-cd $(dirname $0)/../openpose_plus/inference
+paf_install_path="$(dirname $0)/../openpose_plus/inference"
+[ ! -d $paf_install_path ] && mkdir $paf_install_path
+cd $paf_install_path
 
-[ ! -d pafprocess ] && svn export https://github.com/ildoonet/tf-sp-estimation/trunk/tf_pose/pafprocess
-
+[ ! -d pafprocess ] && svn export https://github.com/ildoonet/tf-pose-estimation/trunk/tf_pose/pafprocess
 cd pafprocess
 
 swig -python -c++ pafprocess.i
