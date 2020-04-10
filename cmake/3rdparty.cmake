d98b27962d246a6db2f19e7e2d2b5c11921c001b
lg <lg4869@outlook.com>
2020-04-10 14:23:52 +0100

    try fix #233

--- cmake/3rdparty.cmake
+++ cmake/3rdparty.cmake
@@ -21,16 +21,21 @@ SET(STDTENSOR_GIT_URL
     https://github.com/stdml/stdtensor.git
     CACHE STRING "URL for clone stdtensor")
 
+SET(STDTENSOR_GIT_TAG
+    "7f8a42b188bd75cbcec2a49bb6b9b33779343e3c" # FIXME: use stable tag (v0.9.1)
+                                               # before merge master
+    CACHE STRING "git tag for checkout stdtensor")
+
 EXTERNALPROJECT_ADD(
     stdtensor-repo
     LOG_DOWNLOAD ON
     LOG_INSTALL ON
     LOG_CONFIGURE ON
     GIT_REPOSITORY ${STDTENSOR_GIT_URL}
-    GIT_TAG v0.9.0
+    GIT_TAG ${STDTENSOR_GIT_TAG}
     PREFIX ${THIRDPARTY_PREFIX}
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_PREFIX} -DBUILD_TESTS=0
-               -DBUILD_EXAMPLES=0)
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${THIRDPARTY_PREFIX} -DBUILD_TESTS=0 #
+               -DUSE_STRICT=0 -DBUILD_LIB=0 -DBUILD_EXAMPLES=0)
 
 INCLUDE_DIRECTORIES(${THIRDPARTY_PREFIX}/include)
 
