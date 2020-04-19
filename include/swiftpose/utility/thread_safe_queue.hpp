1c321124f103e2ee17dbd18d6c62dd26b8dfb256
ganler <jaway.liu@gmail.com>
2020-04-20 02:23:43 +0800

    Add test codes in comment of thread_safe_queue

--- include/swiftpose/utility/thread_safe_queue.hpp
+++ include/swiftpose/utility/thread_safe_queue.hpp
@@ -2,6 +2,12 @@
 
 #include <mutex>
 #include <condition_variable>
+#include <vector>
+#include <future>
+#include <optional>
+#include <iostream>
+#include <numeric>
+#include <cassert>
 
 namespace swiftpose {
 
@@ -13,13 +19,13 @@ public:
     {}
 
     ~thread_safe_queue() {
-        std::lock_guard lk{m_mu};
-        delete m_array;
+        delete[] m_array;
     }
 
     void push(T v) {
         std::lock_guard lk{m_mu};
-        m_cv.wait_until(lk, [this](){return !unsafe_is_full();});
+        if (m_size + 1 > m_capacity)
+            throw std::overflow_error("queue size overfloor!");
         m_array[m_back] = std::move(v);
         unsafe_step_back();
         ++m_size;
@@ -28,39 +34,40 @@ public:
     template <typename C>
     void push(C container) {
         std::lock_guard lk{m_mu};
-        m_cv.wait(lk, [this](){return !unsafe_is_full();});
 
-        if (container.size() > m_capacity - m_size)
-            throw std::length_error("Cannot accept so many elements.");
+        if (m_size+container.size() > m_capacity)
+            throw std::overflow_error("queue size overfloor!");
 
         size_t back2cap = m_capacity - m_back;
         if (back2cap >= container.size())
-            std::move(container.begin(), container.end(), m_array + m_head);
+            std::move(container.begin(), container.end(), m_array + m_back);
         else {
             std::move(std::next(container.begin(), back2cap), container.end(), m_array);
-            std::move(container.begin(), std::next(container.begin(), back2cap), m_array + m_head);
+            std::move(container.begin(), std::next(container.begin(), back2cap), m_array + m_back);
         }
 
         unsafe_step_back(container.size());
         m_size += container.size();
     }
 
-    size_t capacity() const noexcept {
+    [[nodiscard]] size_t capacity() const noexcept {
         return m_capacity;
     }
 
-    T dump() {
+    std::optional<T> dump() {
         T ret{};
 
         {
             std::lock_guard lk{m_mu};
+
+            if (m_size == 0)
+                return std::nullopt;
+
             ret = std::move(m_array[m_head]);
 
             unsafe_step_head();
             --m_size;
         }
-
-        m_cv.notify_one();
 
         return ret;
     }
@@ -76,31 +83,23 @@ public:
             for (size_t i = 0; i < len; ++i)
                 ret.push_back(std::move(m_array[(m_head + i) % m_capacity]));
 
-            m_size -= n;
-            unsafe_step_head(n);
+            m_size -= len;
+            unsafe_step_head(len);
         }
-
-        for (size_t j = 0; j < n; ++j)
-            m_cv.notify_one();
 
         return ret;
     }
 
 private:
-    bool unsafe_is_full() const {
-        return m_size == capacity();
-    }
-
     void unsafe_step_back(size_t n = 1) {
         m_back = (m_back + n) % m_capacity;
     }
 
     void unsafe_step_head(size_t n = 1) {
-        m_back = (m_head + n) % m_capacity;
+        m_head = (m_head + n) % m_capacity;
     }
 
     std::mutex              m_mu;
-    std::condition_variable m_cv;
 
     size_t                  m_head;
     size_t                  m_back;
@@ -111,3 +110,97 @@ private:
 };
 
 }
+
+/*
+// Test codes.
+int main() {
+    using namespace swiftpose;
+
+    { // Test 1
+        constexpr size_t iters = 10;
+        thread_safe_queue<std::vector<int>> queue(iters * 3);
+        std::atomic<size_t> answer = 0;
+
+        {
+            std::vector<std::future<void>> vec;
+
+            std::vector template1 = {1, 3, 4};
+            std::vector template2 = {11, 2, 62, 14, 231};
+            std::vector template3 = {13, 534, 12, 5, 31, 2};
+
+            answer += std::accumulate(template1.begin(), template1.end(), 0) * iters;
+            answer += std::accumulate(template2.begin(), template2.end(), 0) * iters;
+            answer += std::accumulate(template3.begin(), template3.end(), 0) * iters;
+
+            vec.push_back(std::async([&]{queue.push(template1);}));
+            vec.push_back(std::async([&]{queue.push(template2);}));
+            vec.push_back(std::async([&]{queue.push(template3);}));
+
+            auto f = std::async([&answer, &queue]{
+                while(answer != 0) {
+                    auto v = queue.dump();
+                    if (v.has_value())
+                        for(auto&& x : v.value())
+                            answer -= x;
+                }
+            });
+
+            for (size_t i = 0; i < iters - 1; ++i) {
+                vec.push_back(std::async([&]{queue.push(template1);}));
+                vec.push_back(std::async([&]{queue.push(template2);}));
+                vec.push_back(std::async([&]{queue.push(template3);}));
+            }
+
+            for(auto&& x : vec)
+                x.get();
+            f.get();
+        }
+
+        assert(answer == 0);
+    }
+
+    { // Test 2
+        constexpr size_t iters = 10;
+        thread_safe_queue<std::vector<int>> queue(iters * 3);
+        std::atomic<size_t> answer = 0;
+
+        {
+            std::vector<std::future<void>> vec;
+
+            std::vector template1 = {1, 3, 4};
+            std::vector template2 = {11, 2, 62, 14, 231};
+            std::vector template3 = {13, 534, 12, 5, 31, 2};
+
+            answer += std::accumulate(template1.begin(), template1.end(), 0) * iters;
+            answer += std::accumulate(template2.begin(), template2.end(), 0) * iters;
+            answer += std::accumulate(template3.begin(), template3.end(), 0) * iters;
+
+            vec.push_back(std::async([&]{queue.push(template1);}));
+            vec.push_back(std::async([&]{queue.push(template2);}));
+            vec.push_back(std::async([&]{queue.push(template3);}));
+
+            auto f = std::async([&answer, &queue]{
+                while(answer != 0) {
+                    auto v = queue.dump(queue.capacity());
+                    for(auto&& x : v) {
+                        for (auto&& y : x)
+                            answer -= y;
+                    }
+                }
+            });
+
+            for (size_t i = 0; i < iters - 1; ++i) {
+                vec.push_back(std::async([&]{queue.push(template1);}));
+                vec.push_back(std::async([&]{queue.push(template2);}));
+                vec.push_back(std::async([&]{queue.push(template3);}));
+            }
+
+            for(auto&& x : vec)
+                x.get();
+            f.get();
+        }
+
+        assert(answer == 0);
+    }
+}
+*/
