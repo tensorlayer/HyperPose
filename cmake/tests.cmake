7ac9302758673ad26c5a287ebd380e96ac740a5a
lg <lg4869@outlook.com>
2020-05-31 18:57:04 +0100

    build tests

--- cmake/tests.cmake
+++ cmake/tests.cmake
@@ -1,19 +1,17 @@
-INCLUDE(${CMAKE_SOURCE_DIR}/cmake/helpers.cmake)
-
-ENABLE_TESTING()
+FIND_PACKAGE(Threads REQUIRED)
 
 FILE(GLOB_RECURSE POSE_TESTS ${CMAKE_SOURCE_DIR}/examples/tests/*.test.cpp)
 
-foreach(TEST_FULL_PATH ${POSE_TESTS})
+FOREACH(TEST_FULL_PATH ${POSE_TESTS})
     MESSAGE(STATUS ">>> [TEST] TO BUILD ${TEST_FULL_PATH}")
     GET_FILENAME_COMPONENT(TEST_NAME ${TEST_FULL_PATH} NAME_WE)
-    # ~ NAME_WE means filename without directory | longest extension
-    # ~ See more details at https://cmake.org/cmake/help/v3.0/command/get_filename_component.html
+    # ~ NAME_WE means filename without directory | longest extension ~ See more
+    # details at
+    # https://cmake.org/cmake/help/v3.0/command/get_filename_component.html
 
     SET(TEST_TAR test.${TEST_NAME})
-    add_executable(${TEST_TAR} ${TEST_FULL_PATH})
-    set_property(
-            TARGET ${TEST_TAR} PROPERTY COMPILE_FLAGS "")
-    target_link_libraries(${TEST_TAR} PRIVATE helpers hyperpose)
-    add_test(NAME ${TEST_TAR} COMMAND ${TEST_TAR})
-endforeach()
+    ADD_EXECUTABLE(${TEST_TAR} ${TEST_FULL_PATH} src/thread_pool.cpp)
+    TARGET_LINK_LIBRARIES(${TEST_TAR} Threads::Threads)
+    SET_PROPERTY(TARGET ${TEST_TAR} PROPERTY COMPILE_FLAGS "")
+    ADD_TEST(NAME ${TEST_TAR} COMMAND ${TEST_TAR})
+ENDFOREACH()
