ec5f733eed53682eb1e20d6a2fc93fc5645b4b2c
ganler <jaway.liu@gmail.com>
2020-06-03 21:09:10 +0800

    ppn init

--- include/hyperpose/operator/parser/proposal_network.hpp
+++ include/hyperpose/operator/parser/proposal_network.hpp
@@ -3,6 +3,21 @@
 namespace hyperpose {
 
 namespace parser {
+
+// The pose_proposal supports the feature map list using the following order:
+
+// 0: predict_confidence N x 18 x 12 x 12
+// 1: predict_iou        N x 18 x 12 x 12
+// 2: x                  N x 18 x 12 x 12
+// 3: y                  N x 18 x 12 x 12
+// 4: w                  N x 18 x 12 x 12
+// 5: h                  N x 18 x 12 x 12
+// 6: edge_confidence    N x 17 x 9 x 9 x 12 x 12
+
+class pose_proposal {
+
+};
+
 }
 
 } // namespace hyperpose
