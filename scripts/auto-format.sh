96840983cca68aa38c05c65f8e75cf42b9e6d436
ganler <jaway.liu@gmail.com>
2021-06-27 20:39:42 +0800

    feat: add a Python2/3 compatible and dependency-free dependency checker for container execution

--- scripts/auto-format.sh
+++ scripts/auto-format.sh
@@ -2,13 +2,13 @@ set -e
 
 export CLANG_FORMAT=clang-format
 
-[ $(which $CLANG_FORMAT) ] || python3 -m pip install clang-format
+command -v $CLANG_FORMAT || python3 -m pip install clang-format
 
 format_dir() {
-    find $1 -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec $CLANG_FORMAT -style=file -i {} \;
+    find "$1" -regex '.*\.\(cpp\|hpp\|cc\|cxx\)' -exec $CLANG_FORMAT -style=file -i {} \;
 }
 
-cd $(dirname $0)/..
+cd "$(dirname "$0")"/..
 
 format_dir ./examples
 format_dir ./include
