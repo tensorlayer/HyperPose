a487d1f1456cc057285c9ada4b3173194f77bfdb
ganler <jaway.liu@gmail.com>
2021-07-01 00:04:28 +0800

    feat: auto glob tensorflow GraphDef input/output nodes

--- export_pb.py
+++ export_pb.py
@@ -3,9 +3,27 @@
 import os
 import argparse
 import tensorflow as tf
-import tensorlayer as tl
-from hyperpose import Config,Model,Dataset
+from hyperpose import Config, Model
 from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
+def analyze_inputs_outputs(graph):
+    ops = graph.get_operations()
+    outputs_set = set(ops)
+    inputs = []
+    for op in ops:
+        if len(op.inputs) == 0 and op.type == 'Placeholder':
+            inputs.append(op)
+        else:
+            for input_tensor in op.inputs:
+                if input_tensor.op in outputs_set:
+                    outputs_set.remove(input_tensor.op)
+    outputs = list(outputs_set)
+    # Control nodes shall not be considered.
+    # input like: "import/x" -> x
+    # output like: "import/Identity", "import/Identity_1" -> Identity, Identity_1
+    inputs = [x.name.split('/')[-1] for x in inputs if '_control_node' not in x.name]
+    outputs = [x.name.split('/')[-1] for x in outputs if '_control_node' not in x.name]
+    return (inputs, outputs)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='export fastpose models to pb format.')
@@ -54,17 +72,17 @@ if __name__ == '__main__':
 
     export_batch_size=args.export_batch_size
     export_h,export_w=args.export_h,args.export_w
-    print(f"export_batch_size:{export_batch_size} export_h:{export_h} export_w:{export_w}")
+    print(f"{export_batch_size = }\t{export_h = }\t{export_w = }")
     input_path=f"{config.model.model_dir}/newest_model.npz"
     output_dir=f"{args.output_dir}/{config.model.model_name}"
     output_path=f"{output_dir}/frozen_{config.model.model_name}.pb"
-    print(f"exporting model {config.model.model_name} from {input_path}...")
+    print(f"Exporting model {config.model.model_name} from {input_path}...")
     if(not os.path.exists(output_dir)):
-        print("creating output_dir...")
+        print("Creating output_dir...")
         os.mkdir(output_dir)
     if(not os.path.exists(input_path)):
-        print("input model file doesn't exist!")
-        print("conversion aborted!")
+        print("Input model file doesn't exist!")
+        print("Conversion aborted!")
     else:
         export_model.load_weights(input_path)
         export_model.eval()
@@ -77,5 +95,10 @@ if __name__ == '__main__':
         frozen_graph_def=frozen_graph.graph.as_graph_def()
         tf.io.write_graph(graph_or_graph_def=frozen_graph_def,logdir=output_dir,name=f"frozen_{args.model_name}.pb",\
             as_text=False)
-        print(f"exporting pb file finished! output file: {output_path}")
+        print(f"Exporting pb file finished! output file: {output_path}")
 
+        with tf.Graph().as_default()  as graph:
+            tf.import_graph_def(frozen_graph_def)
+            input_names, output_names = analyze_inputs_outputs(graph)
+            print(f'Exported graph INPUT nodes: {input_names}')
+            print(f'Exported graph OUTPUT nodes: {output_names}')
