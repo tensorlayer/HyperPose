14961243e9679215038d8dda403f039b1160f731
ganler <jaway.liu@gmail.com>
2021-06-30 15:59:08 +0800

    refact: resolve `html_static_path` warning

--- docs/conf.py
+++ docs/conf.py
@@ -80,7 +80,7 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 master_doc = 'index'
 
